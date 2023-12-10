e1d505260a23e858aa83ebdce1e3bd0806bc3111
Roman Kudravcev <roman.kudravcev@stud.uni-bamberg.de>
2023-12-10 22:46:13 +0100

    add cors

--- backend/main.go
+++ backend/main.go
@@ -2,12 +2,30 @@ package main
 
 import "github.com/gin-gonic/gin"
 
+func CORSMiddleware() gin.HandlerFunc {
+    return func(c *gin.Context) {
+        c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+        c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+        c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+        c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+        if c.Request.Method == "OPTIONS" {
+            c.AbortWithStatus(204)
+            return
+        }
+
+        c.Next()
+    }
+}
+
 func main() {
     // Create a Gin router with default middleware: logger and recovery (crash-free) middleware
     router := gin.Default()
+	router.Use(CORSMiddleware())
+
 
     // Define a route
-    router.GET("/", func(c *gin.Context) {
+    router.GET("/api/data", func(c *gin.Context) {
         c.JSON(200, gin.H{
             "message": "Hello, world!",
         })
